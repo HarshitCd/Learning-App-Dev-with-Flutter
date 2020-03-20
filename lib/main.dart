98cb1959e8d0a39c5ca3900871039e4001b59f11
HarshitCd <harshitcd@gmail.com>
2020-03-20 08:38:43 +0530

    Lesson10 Made an Id Card

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,124 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
-
+  home: Id_Home(),
 ));
 
-class Home extends StatelessWidget {
+class Id_Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[900],
       appBar: AppBar(
-        title: Text('Hello World'),
+        title: Text('ID Card'),
         centerTitle: true,
-        backgroundColor: Colors.red[200],
+        backgroundColor: Colors.amber,
       ),
-      body: Row(
-        children: <Widget>[
-          Expanded(
-              flex: 4,
-              child: Image.asset('assets/1img.jpg')
-          ),
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.red[300],
-              child: Text('1'),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.grey[800],
+                child: Image.asset('assets/greninja.png'),
+                radius: 50,
+              ),
             ),
-          ),
-
-          Expanded(
-            flex: 2,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.green[300],
-              child: Text('2'),
+            Divider(
+              height: 60,
+              color: Colors.grey[850],
             ),
-          ),
-
-          Expanded(
-            flex: 1,
-            child: Container(
-              padding: EdgeInsets.all(30.0),
-              color: Colors.blue[300],
-              child: Text('3'),
+            Text(
+              'Name:',
+              style: TextStyle(
+                letterSpacing: 1,
+                fontSize: 20,
+                color: Colors.grey[700],
+              ),
             ),
-          ),
-
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Click'),
-        backgroundColor: Colors.red[300],
+            SizedBox(height: 5),
+            Text(
+              'Greninja',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.amber[700],
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Class:',
+              style: TextStyle(
+                letterSpacing: 1,
+                fontSize: 20,
+                color: Colors.grey[700],
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              '4th Sem - A',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.amber[700],
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Department:',
+              style: TextStyle(
+                letterSpacing: 1,
+                fontSize: 20,
+                color: Colors.grey[700],
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              'Information Science',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.amber[700],
+              ),
+            ),
+            SizedBox(height: 30),
+            Text(
+              'Age:',
+              style: TextStyle(
+                letterSpacing: 1,
+                fontSize: 20,
+                color: Colors.grey[700],
+              ),
+            ),
+            SizedBox(height: 5),
+            Text(
+              '18',
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.amber[700],
+              ),
+            ),
+            SizedBox(height: 30),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.mail,
+                  size: 30,
+                  color: Colors.grey[400],
+                ),
+                SizedBox(width: 10),
+                Text(
+                  'greninja.is18@bmsce.ac.in',
+                  style: TextStyle(
+                    letterSpacing: 1,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[400],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
