bd688d7d25c81dbac2f2ab08d8fb2df5c74e7fed
HarshitCd <harshitcd@gmail.com>
2020-03-21 17:31:22 +0530

    Lesson16 Functions as Parameters

--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -4,36 +4,43 @@ import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
   final Quote quote;
-  QuoteCard({ this.quote });
+  final Function delete;
+  QuoteCard({ this.quote, this.delete });
   @override
   Widget build(BuildContext context) {
     return Card(
       color: Colors.grey[200],
       margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+
       child: Padding(
-        padding: EdgeInsets.all(12),
+        padding: EdgeInsets.all(12.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18,
-                color: Colors.grey[600],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                quote.text,
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.grey[600],
+                ),
               ),
-            ),
-            SizedBox(height: 6, width: 3,),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14,
-                color: Colors.grey[700],
+              SizedBox(height: 6, width: 3,),
+              Text(
+                quote.author,
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[700],
+                ),
               ),
-            ),
-            SizedBox(height: 6, width: 3,),
-          ],
-        ),
+              SizedBox(height: 6, width: 3,),
+              IconButton(
+                onPressed: delete,
+                iconSize: 18,
+                icon: Icon(Icons.delete),
+              ),
+            ],
+          ),
       ),
     );
   }
